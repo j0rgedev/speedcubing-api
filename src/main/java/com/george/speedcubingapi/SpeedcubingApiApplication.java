70fa5eef64e09cdbafefae48bfb2682225f506dc
george <jorgeoah.20@outlook.com>
2023-11-23 14:40:39 -0500

    First commit

---
+++ src/main/java/com/george/speedcubingapi/SpeedcubingApiApplication.java
@@ -0,0 +1,13 @@
+package com.george.speedcubingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedcubingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedcubingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/george/speedcubingapi/SpeedcubingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.speedcubingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedcubingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
